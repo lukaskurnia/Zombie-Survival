0c42234fe9108dbb0117cbb3cc98cb25f984634e
lukaskurnia <lukasjonathan99@gmail.com>
2020-03-31 00:39:29 +0700

    finish enemy gameflow

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,51 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public int health = 100;
-    const int scoreValue = 100;
 
+    public static int numOfEnemies = 0;
+    public int health = 100;
+    private const int scoreValue = 100;
     public GameObject EnemyDeath;
+
+    //Movement
+    private float speed = 0.8f;
+    private bool facingRight = true;
+
+    private Animator animator;
+    private Rigidbody2D rigidBody;
+
+    private void Awake() {
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    void Update() { 
+        if(rigidBody.velocity.y == 0) {            
+            animator.SetBool("isWalking", true);
+            animator.SetBool("isAttack", false);
+        }     
+        transform.Translate(2 * Time.deltaTime * speed, 0.0f,0.0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D info) {
+        if(info.gameObject.CompareTag("turn_back") || info.gameObject.CompareTag("zombie")){
+            Flip();
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D info) {
+        Debug.Log(info);  
+        PlayerController player = info.GetComponent<PlayerController>();
+        if(player != null){ //hit an enemy
+            animator.SetBool("isAttack", true);
+            animator.SetBool("isWalking", false);
+        }                    
+    }
+
+    private void Flip() {
+        facingRight = !facingRight;
+        transform.Rotate(0f,180f,0f);
+    }
+
     public void TakeDamage(int damage) {
         health -= damage;
 
@@ -19,6 +60,7 @@ public class Enemy : MonoBehaviour
     void Die() {
         Instantiate(EnemyDeath, transform.position, Quaternion.identity);
         Score.score += scoreValue;
+        numOfEnemies--;
         Destroy(gameObject);
     }
 
---
+++ Assets/Scripts/ZombieSpawners.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawners : MonoBehaviour
+{
+    public GameObject enemy;
+    float randomX, randomY;
+    Vector2 spawnPoint;
+    public float spawnRate = 5f;
+    float nextSpawn = 0f;
+    const int MAX_ENEMY = 20;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextSpawn && Enemy.numOfEnemies < MAX_ENEMY) {
+            nextSpawn = Time.time + spawnRate;
+            randomX = Random.Range(-28f,27f);
+
+            if(randomX  < -18f) randomY = 2.4f;
+            else if(randomX < -9f && randomX >-16f) randomY = 6.1f;
+            else if(randomX > -3f && randomX < 9f) randomY = 4.5f;
+            else if(randomX > 14f && randomX < 20f) randomY = 4.5f;
+            else randomY = 14.5f;            
+
+            spawnPoint = new Vector2(randomX,randomY);
+            Instantiate(enemy, spawnPoint, Quaternion.identity);
+            Enemy.numOfEnemies++;
+        }
+
+    }
+}
